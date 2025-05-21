d5f12e24cbda2a0292674f15dc90aace22d7aa36
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-21 12:54:44 +0330

    add simple Post model and edit blog_ap/models/__init__.py

---
+++ blog_app/models/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    'Post',
+]
+
+from blog_app.models.post import Post
---
+++ blog_app/models/post.py
@@ -0,0 +1,11 @@
+from django.db import models
+from blog_app.models.custom_user_model import TimeStampModel
+
+class Post(TimeStampModel):
+    content = models.TextField()     
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    is_published = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.title}'
