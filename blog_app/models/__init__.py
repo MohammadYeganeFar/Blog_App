371ec2426a3d748d34e8c8b940f5763f6527e434
Farbod Nezafati <farbodneza@gmail.com>
2025-05-21 11:31:27 +0330

    import reader_model into __init__ and fix absolute addresses

--- blog_app/models/__init__.py
+++ blog_app/models/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ['CustomUserModel', 'TimeStampModel']
+__all__ = ['CustomUserModel', 'TimeStampModel', 'ReaderModel']
 
-
-from custom_user_model import CustomUserModel
-from custom_user_model import TimeStampModel
+from blog_app.models.reader_model import ReaderModel
+from blog_app.models.custom_user_model import CustomUserModel
+from blog_app.models.custom_user_model import TimeStampModel
