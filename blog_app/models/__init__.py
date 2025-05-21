cd38569e0800aad0293cc6f4e9d7d943bb346d9c
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-21 22:09:57 +0330

    rename Post -> PostModel in blog_app/models/post_model

--- blog_app/models/__init__.py
+++ blog_app/models/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ['CustomUserModel', 'TimeStampModel', 'ReaderModel', 'Post']
+__all__ = ['CustomUserModel', 'TimeStampModel', 'ReaderModel', 'PostModel']
 
 from blog_app.models.reader_model import ReaderModel
 from blog_app.models.custom_user_model import CustomUserModel
 from blog_app.models.custom_user_model import TimeStampModel
-from blog_app.models.post_model import Post
+from blog_app.models.post_model import PostModel
