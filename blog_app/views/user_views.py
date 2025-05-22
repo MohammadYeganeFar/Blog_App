00d3083619f4ab7021dd727013a3bf8cce43336c
Farbod Nezafati <farbodneza@gmail.com>
2025-05-22 16:27:18 +0330

    add user login form and half of custom_user_login view

---
+++ blog_app/views/user_views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from blog_app.forms.user.login import CustomLoginForm
+
+
+def custom_user_login(request):
+    if request.method == 'POST':
+        form = custom_user_login(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get(username)
+            password = form.cleaned_data.get(password)
+            user = authenticate(
+            request=request,
+            username=username,
+            password=password
+            )
