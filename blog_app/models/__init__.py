7e29414880aa701afd3a4d1fb2a6f850f77cd4f3
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-22 15:29:30 +0330

    remove AuthorModel and ReaderModel and update CustomUserModel
    CustomUserModel:
        - remove unneccery fields
        + add user, profile_image and bio fields

--- blog_app/models/__init__.py
+++ blog_app/models/__init__.py
@@ -1,11 +1,9 @@
 __all__ = [
     'CustomUserModel',
     'TimeStampModel',
-    'ReaderModel',
     'PostModel',
     'ContactUsModel']
 
-from blog_app.models.reader_model import ReaderModel
 from blog_app.models.custom_user_model import CustomUserModel
 from blog_app.models.custom_user_model import TimeStampModel
 from blog_app.models.contact_us_model import ContactUsModel
--- blog_app/models/author_model.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from blog_app.models import CustomUserModel
-
-
-class AuthorModel(CustomUserModel):
-    bio = models.TextField(blank=True,null=True)
-    
-    def __str__(self):
-        return f"Author: {self.username}"
