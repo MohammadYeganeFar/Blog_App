3660ad4413f6fa32962fbb1de4d04cc02f037de8
Farbod Nezafati <farbodneza@gmail.com>
2025-05-24 21:56:15 +0330

    create post and user_registration forms and add them to forms.__init__.py

--- blog_app/forms/__init__.py
+++ blog_app/forms/__init__.py
@@ -1,8 +1,13 @@
 __all__ = ['ContactUs',
            'Comment',
+           'UserProfile',
+           'Post',
            'UserProfile']
 
 from blog_app.forms.contact_us import ContactUs
 from blog_app.forms.comment import Comment
 from blog_app.forms.user_profile import UserProfile
+from blog_app.forms.post import Post
+from blog_app.forms.user_profile import UserProfile
+
 
