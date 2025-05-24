ff85c196b558bb0b6f177c0a405e23a143ebd9a6
zavare <r.zavareh26@gmail.com>
2025-05-24 20:31:55 +0330

    refactor blog_app.models.__init__.py

--- blog_app/models/__init__.py
+++ blog_app/models/__init__.py
@@ -3,10 +3,16 @@ __all__ = [
     'TimeStampModel',
     'Post',
     'ContactUs',
-    'Comment']
+    'Comment',
+    'Tag',
+    'Like',
+    'BaseAdmin']
 
 from blog_app.models.custom_user import CustomUser
 from blog_app.models.custom_user import TimeStampModel
 from blog_app.models.contact_us import ContactUs
 from blog_app.models.post import Post
 from blog_app.models.comment import Comment
+from blog_app.models.tag import Tag
+from blog_app.models.comment import Like
+from blog_app.models.base_admin import BaseAdmin
