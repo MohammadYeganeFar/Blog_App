c1bf3c5fb12344d738f53d801f29091d4065bbbc
Farbod Nezafati <farbodneza@gmail.com>
2025-05-24 15:13:31 +0330

    add comment model and add it into models.__init__.py

--- blog_app/models/__init__.py
+++ blog_app/models/__init__.py
@@ -2,9 +2,11 @@ __all__ = [
     'CustomUser',
     'TimeStampModel',
     'PostModel',
-    'ContactUs']
+    'ContactUs',
+    'Comment']
 
 from blog_app.models.custom_user import CustomUser
 from blog_app.models.custom_user import TimeStampModel
 from blog_app.models.contact_us import ContactUs
 from blog_app.models.post import Post
+from blog_app.models.comment import Comment
---
+++ blog_app/models/comment.py
@@ -0,0 +1,14 @@
+from django.db import models
+from blog_app.models.post import PostModel
+from blog_app.models.custom_user import TimeStampModel, CustomUser
+
+
+class Comment(TimeStampModel):
+    post = models.ForeignKey(PostModel, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
+    content = models.TextField()
+
+
+class Like(TimeStampModel):
+    post = models.ForeignKey(PostModel, related_name='likes', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
