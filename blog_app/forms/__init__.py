f2cba18a67889743c1509ee3e6e21c2edcb209b7
Farbod Nezafati <farbodneza@gmail.com>
2025-05-24 20:13:20 +0330

    create user_profile form and add it into forms.__init__.py, fix contact us name

--- blog_app/forms/__init__.py
+++ blog_app/forms/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ['ContactUsForm',
-           'Comment']
+__all__ = ['ContactUs',
+           'Comment',
+           'UserProfile']
 
-from blog_app.forms.contact_us import ContactUsForm
+from blog_app.forms.contact_us import ContactUs
 from blog_app.forms.comment import Comment
+from blog_app.forms.user_profile import UserProfile
 
