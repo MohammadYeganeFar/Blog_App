eeaf51cc48b62298011fe21aa74e0865f65a16b2
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-25 17:29:49 +0330

    fix: remove wrong imports and import correctly in`__init__.py`

--- blog_app/models/__init__.py
+++ blog_app/models/__init__.py
@@ -10,7 +10,4 @@ __all__ = [
 from blog_app.models.custom_user import CustomUser
 from blog_app.models.custom_user import TimeStampModel
 from blog_app.models.contact_us import ContactUs
-from blog_app.models.post import Post
-from blog_app.models.comment import Comment
-from blog_app.models.comment import Like
-from blog_app.models.tag import Tag
+from blog_app.models.post import Post, Comment, Like, Tag
