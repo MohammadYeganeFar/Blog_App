ffa09ad1dbc714fa77d14ed8ea9338e546971341
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-25 14:11:36 +0330

    refactor: typo in `UserRegistratio`.  changed to `UserRegistration`

--- blog_app/forms/user_registration.py
+++ blog_app/forms/user_registration.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from blog_app.models.custom_user import CustomUser
 
 
-class UserRegistratio(forms.ModelForm):
+class UserRegistration(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput, label="Password")
     password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
