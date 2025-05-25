308e7f75eda077adfe502c181f9c48ab3a4f8593
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-25 17:00:33 +0330

    fix:  Comment, Like, Tag to models.__init__.py

--- blog_app/models/__init__.py
+++ blog_app/models/__init__.py
@@ -3,10 +3,14 @@ __all__ = [
     'TimeStampModel',
     'Post',
     'ContactUs',
-    'Comment']
+    'Comment',
+    'Like',
+    'Tag']
 
 from blog_app.models.custom_user import CustomUser
 from blog_app.models.custom_user import TimeStampModel
 from blog_app.models.contact_us import ContactUs
 from blog_app.models.post import Post
 from blog_app.models.comment import Comment
+from blog_app.models.comment import Like
+from blog_app.models.tag import Tag
