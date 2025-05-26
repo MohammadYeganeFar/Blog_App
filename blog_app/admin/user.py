3aaacf88aaca3cc809705fe27cced6547bd12012
zavare <r.zavareh26@gmail.com>
2025-05-26 20:45:01 +0330

    Update field in blog_app/admin/user.py/CustomUserAdmin

--- blog_app/admin/user.py
+++ blog_app/admin/user.py
@@ -5,6 +5,6 @@ from blog_app.models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseAdmin):
-    list_display = ('user_name', 'first_name', 'last_name', 'email', 'is_staff', 'date_joined', 'is_active' )
-    search_fields = ('user_name', 'first_name__icontains', 'last_name__icontains', 'email', 'date_joined', 'is_active')  
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'date_joined', 'is_active' )
+    search_fields = ('username', 'first_name__icontains', 'last_name__icontains', 'email', 'date_joined', 'is_active')  
     list_filter = ('is_staff', 'date_joined', 'is_active')
