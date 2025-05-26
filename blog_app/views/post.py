e55f27a83fc7ea46389f3268e83afd3d7f7215a4
Mohammad Hossein Soltani <soltanisoltanegharb@gmail.com>
2025-05-26 20:46:31 +0330

    Feature: add like_post to blog_app.views.post

--- blog_app/views/post.py
+++ blog_app/views/post.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from blog_app.models import Post, Like
-=======
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from blog_app.models.post import Post
->>>>>>> f09a81feb885f42077723427f07ccb6c14e777b3
 
 
 def post_list(request):
@@ -20,7 +14,6 @@ def post_list(request):
         context
     )
 
-<<<<<<< HEAD
 
 @login_required
 @require_POST
@@ -38,12 +31,3 @@ def like_post(request, slug):
     like_count = post.likes.count()
 
     return JsonResponse({'liked':liked, 'like_count': like_count})
-=======
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    return render(
-        request,
-        'blog_app/post/detail.html',
-        context={'post': post}
-    )
->>>>>>> f09a81feb885f42077723427f07ccb6c14e777b3
