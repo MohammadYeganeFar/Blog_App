46644c071f9396499f53e37453f76760b223f975
Mohammad Hossein Soltani <soltanisoltanegharb@gmail.com>
2025-05-26 15:49:58 +0330

    fix: in blog_app.admin.py username was written wrongly user_name

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -4,8 +4,8 @@ from blog_app.models import CustomUser, Post, Comment, Tag, Like, BaseAdmin
 
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseAdmin):
-    list_display = ('user_name', 'first_name', 'last_name', 'email', 'is_staff', 'date_joined', 'is_active' )
-    search_fields = ('user_name', 'first_name__icontains', 'last_name__icontains', 'email', 'date_joined', 'is_active')  
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'date_joined', 'is_active' )
+    search_fields = ('username', 'first_name__icontains', 'last_name__icontains', 'email', 'date_joined', 'is_active')  
     list_filter = ('is_staff', 'date_joined', 'is_active')
 
  
