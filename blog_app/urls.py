7ed097423efde87bc64e096e08cac01076a0ef2a
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-26 16:33:07 +0330

    fix: correct blog_app/urls.py considering restructuring views folder

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog_app.views import post_views, user_views
+from blog_app.views import post
+from blog_app.views import user
 
 
 urlpatterns = [
-    path('posts/', post_views.list_post, name='list_post'),
-    path('login/', user_views.custom_user_login, name='login')
+    path('posts/', post.list_post, name='list_post'),
+    path('login/', user.custom_user_login, name='login')
 ]
