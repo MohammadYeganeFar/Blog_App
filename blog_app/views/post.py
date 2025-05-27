7dfe87e7d0206147c743bcfc77fc4009c4e99fda
Mohammad Hossein Soltani <soltanisoltanegharb@gmail.com>
2025-05-27 20:34:53 +0330

    Feature: Refactor to Add pagination to post view

--- blog_app/views/post.py
+++ blog_app/views/post.py
@@ -3,16 +3,24 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from blog_app.models import Post, Like
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(
-        request,
-        'post/post_list.html',
-        context
-    )
+    all_posts_list = Post.objects.filter(status='published').order_by('-created_at')
+    paginator = Paginator(all_posts_list, per_page=10)
+
+    page_number = request.GET.get('page')
+    try:
+        posts_page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts_page_obj = paginator.page(1)
+    except EmptyPage:
+        posts_page_obj = paginator.page(paginator.num_pages)
+
+    context = {'page_obj': posts_page_obj}
+    
+    return render(request, 'blog_app/post/post_list.html', context)
 
 
 @login_required
