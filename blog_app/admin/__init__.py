5d021b2f08484ec6de38b9a0ceba39fec592978c
zavare <r.zavareh26@gmail.com>
2025-05-27 22:50:04 +0330

    Update: added new imports and modifications to blog_app/admin/__init__.py

---
+++ blog_app/admin/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['BaseAdmin', 'PostAdmin', 'LikeAdmin', 'TagAdmin', 'CustomUserAdmin'] 
+from blog_app.admin.base_admin import BaseAdmin
+from blog_app.admin.post import PostAdmin
+from blog_app.admin.post import LikeAdmin
+from blog_app.admin.post import TagAdmin
+from blog_app.admin.user import CustomUserAdmin
