35e1c3fd1de23c59439e2adf02e956522c5b49fe
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-28 13:42:25 +0330

    feature: edit_post view

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('posts/', post.post_list, name='post_list'),
     path('login/', user.custom_user_login, name='login'),
     path('@<str:user>/<slug:slug>/', post.post_detail, name='post_detail'),
-    path('new/', post.create_post, name='create_post')
+    path('new/', post.create_post, name='create_post'),
+    path('<slug:slug>/edit/', post.edit_post, name='edit_post')
 ]
--- blog_app/views/post.py
+++ blog_app/views/post.py
@@ -4,9 +4,21 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.urls import reverse
-from blog_app.models import Post, Like
+from blog_app.models import Post, Like, Tag
 from blog_app.forms.post import Post as PostForm
 
+
+def clean_tags(tag_string):
+    return [tag.strip() for tag in tag_string.split(',') if tag.strip()]
+
+def set_tags(tags_list, post):
+    tags_objects = []
+    print(f"tags_list: {tags_list}\n\nclean_tags_list: {tags_list}")
+    for tag_name in tags_list:
+        tag_object, created = Tag.objects.get_or_create(tag_name=tag_name)
+        tags_objects.append(tag_object)
+    print(f'tags_objects: {tags_objects}')
+    post.tags.set(tags_objects)
 
 def create_post(request):
     user = request.user
@@ -17,6 +29,12 @@ def create_post(request):
             post = form.save(commit=False)
             post.author = user
             post.save()
+            # here we are adding tags to the post
+            user_tags = form.cleaned_data['tags']
+            clean_tags_list = clean_tags(user_tags)
+            set_tags(clean_tags_list, post)
+            post.save()
+            print(f'post.tags: {post.tags.all()}')
             return redirect(
                 reverse(
                     'blog_app:post_detail',
@@ -36,6 +54,35 @@ def create_post(request):
             {'form': form}
         )
 
+def edit_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    user = request.user
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            # here we are adding tags to the post
+            user_tags = form.cleaned_data['tags']
+            clean_tags_list = clean_tags(user_tags)
+            set_tags(clean_tags_list, post)
+            post.save()
+            # I always forget "returen" in "return redirect"
+            return redirect(reverse('blog_app:post_detail', args=[user.username, post.slug]))
+        else:
+            return render(
+                request,
+                'blog_app/post/create_post.html',
+                {'form': form}
+            )
+    else:
+        form = PostForm(instance=post)
+        return render(
+                request,
+                'blog_app/post/create_post.html',
+                {'form': form}
+            )
         
 def post_list(request):
     posts = Post.objects.all()
