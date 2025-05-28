1741f795f5dc0af5a11abfbc322e219ef3d51cd1
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-28 15:09:42 +0330

    fix all import errors in forms/__init__.py and blog_app/usrls.py

--- blog_app/forms/__init__.py
+++ blog_app/forms/__init__.py
@@ -1,11 +1,8 @@
 __all__ = ['ContactUs',
            'CommentForm',
            'UserProfile',
-           'Post',
-           'UserProfile']
+           'Post']
 
 from blog_app.forms.contact_us import ContactUs
-from blog_app.forms.comment import CommentForm
-from blog_app.forms.user_profile import UserProfile
-from blog_app.forms.post import Post
-from blog_app.forms.user_profile import UserProfile
+from blog_app.forms.post import Post, CommentForm
+from blog_app.forms.user import UserProfile
